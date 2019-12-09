44d1001b5ca68d4bcbbdb1d9db445594518eee6d
Zeppelin03 <c17478586@mytudublin.ie>
2019-12-09 22:55:26 +0000

    Added Colour Changer + Music

---
+++ Assets/Scripts/ColourChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChanger : MonoBehaviour
+{
+    AudioSource audioSource;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(audioSource.pitch > 0)
+        {
+            rend.material.SetColor("_color", Random.ColorHSV());
+        }
+    }
+}
